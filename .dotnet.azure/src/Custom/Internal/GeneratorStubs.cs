ba95e3c7cc6a9343efbe35a6952f2fa4f8f9d0da
Ralph <18542438+ralph-msft@users.noreply.github.com>
2024-09-04 12:38:49 -0700

    Sync Azure OpenAI code between this and Azure repo (#228)
    - Refactors the code to bring the directory structure in sync with what we have in the Azure repo
    - Updates the Azure OpenAI code with all changes from the Azure repo
    - Brings over the Azure OpenAI test suite to allow for full testing in this repo (including support for recording/playback of requests)
    - Fixes a regression introduced with the latest FineTuningClient pagination changes
    
    Co-authored-by: Travis Wilson <travisw@microsoft.com>

--- .dotnet.azure/src/Custom/Internal/GeneratorStubs.cs
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-namespace Azure.AI.OpenAI;
-
-[CodeGenModel("AzureOpenAIChatErrorResponse")] internal partial class AzureOpenAIChatErrorResponse { }
-
-[CodeGenModel("AzureOpenAIDalleErrorResponse")] internal partial class AzureOpenAIDalleErrorResponse { }
